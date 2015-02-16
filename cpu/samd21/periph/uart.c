db34e9a0cd8372139ab4e3ffd49d62e3b9930794
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2015-02-16 11:30:13 +0100

    peter adds HWSEL

--- cpu/samd21/periph/uart.c
+++ cpu/samd21/periph/uart.c
@@ -101,7 +101,7 @@ int uart_init_blocking(uart_t uart, uint32_t baudrate)
     uint32_t tx_pin = 0;
     uint32_t rx_pin = 0;
     uint32_t pins = 0;
-
+    uint32_t HWSEL = 0;
 
     switch (uart) {
 #if UART_0_EN
@@ -112,6 +112,7 @@ int uart_init_blocking(uart_t uart, uint32_t baudrate)
             tx_pin = UART_0_TX_PIN;
             rx_pin = UART_0_RX_PIN;
             pins = UART_0_PINS;
+            HWSEL = 0;
 
             /* Turn on power manager for sercom0 */
             PM->APBCMASK.reg |= PM_APBCMASK_SERCOM0;
@@ -136,6 +137,7 @@ int uart_init_blocking(uart_t uart, uint32_t baudrate)
             tx_pin = UART_1_TX_PIN;
             rx_pin = UART_1_RX_PIN;
             pins = UART_1_PINS;
+            HWSEL = (PORT_WRCONFIG_HWSEL);
 
             /* Turn on power manager for sercom5 */
             PM->APBCMASK.reg |= PM_APBCMASK_SERCOM5;
@@ -171,7 +173,8 @@ int uart_init_blocking(uart_t uart, uint32_t baudrate)
 
 
     /* enable PMUX for pins and set to config D. See spec p. 12 */
-    port_group->WRCONFIG.reg = PORT_WRCONFIG_WRPINCFG     /* PINCFGy registers of the selected pins will be updated */
+    port_group->WRCONFIG.reg = HWSEL
+                                | PORT_WRCONFIG_WRPINCFG     /* PINCFGy registers of the selected pins will be updated */
                                 | PORT_WRCONFIG_WRPMUX    /* PMUXn registers of the selected pins will be updated */
                                 | PORT_WRCONFIG_PMUX(0x3) /* Multiplexer peripheral function D */
                                 | PORT_WRCONFIG_PMUXEN    /* Peripheral multiplexer enable */
