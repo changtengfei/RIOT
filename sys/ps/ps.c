db8fdba419747969c336b858daeca59e09d0304c
PeterKietzmann <peter.kietzmann@haw-hamburg.de>
2016-03-04 16:26:34 +0100

    simplify ps module

--- sys/ps/ps.c
+++ sys/ps/ps.c
@@ -43,74 +43,73 @@ const char *state_names[] = {
  */
 void ps(void)
 {
-    const char queued_name[] = {'_', 'Q'};
+// const char queued_name[] = {'_', 'Q'};
 #ifdef DEVELHELP
     int overall_stacksz = 0, overall_used = 0;
 #endif
 
-    printf("\tpid | "
-#ifdef DEVELHELP
-            "%-21s| "
-#endif
-            "%-9sQ | pri "
-#ifdef DEVELHELP
-           "| stack ( used) | location   "
-#endif
-#ifdef MODULE_SCHEDSTATISTICS
-           "| runtime | switches"
-#endif
-           "\n",
-#ifdef DEVELHELP
-           "name",
-#endif
-           "state");
+//     printf("\tpid | "
+// #ifdef DEVELHELP
+//             "%-21s| "
+// #endif
+//             "%-9sQ | pri "
+// #ifdef DEVELHELP
+//            "| stack ( used) | location   "
+// #endif
+// #ifdef MODULE_SCHEDSTATISTICS
+//            "| runtime | switches"
+// #endif
+//            "\n",
+// #ifdef DEVELHELP
+//            "name",
+// #endif
+//            "state");
 
     for (kernel_pid_t i = KERNEL_PID_FIRST; i <= KERNEL_PID_LAST; i++) {
         tcb_t *p = (tcb_t *)sched_threads[i];
 
         if (p != NULL) {
-            int state = p->status;                                                 /* copy state */
-            const char *sname = state_names[state];                                /* get state name */
-            const char *queued = &queued_name[(int)(state >= STATUS_ON_RUNQUEUE)]; /* get queued flag */
+            // int state = p->status;                                                 /* copy state */
+//            const char *sname = state_names[state];                                /* get state name */
+//            const char *queued = &queued_name[(int)(state >= STATUS_ON_RUNQUEUE)]; /* get queued flag */
 #ifdef DEVELHELP
             int stacksz = p->stack_size;                                           /* get stack size */
             overall_stacksz += stacksz;
             stacksz -= thread_measure_stack_free(p->stack_start);
             overall_used += stacksz;
 #endif
-#ifdef MODULE_SCHEDSTATISTICS
-            double runtime_ticks =  sched_pidlist[i].runtime_ticks / (double) xtimer_now() * 100;
-            int switches = sched_pidlist[i].schedules;
-#endif
-            printf("\t%3" PRIkernel_pid
-#ifdef DEVELHELP
-                   " | %-20s"
-#endif
-                   " | %-8s %.1s | %3i"
-#ifdef DEVELHELP
-                   " | %5i (%5i) | %p "
-#endif
-#ifdef MODULE_SCHEDSTATISTICS
-                   " | %6.3f%% |  %8d"
-#endif
-                   "\n",
-                   p->pid,
-#ifdef DEVELHELP
-                   p->name,
-#endif
-                   sname, queued, p->priority
-#ifdef DEVELHELP
-                   , p->stack_size, stacksz, p->stack_start
-#endif
-#ifdef MODULE_SCHEDSTATISTICS
-                   , runtime_ticks, switches
-#endif
-                  );
+// #ifdef MODULE_SCHEDSTATISTICS
+//             double runtime_ticks =  sched_pidlist[i].runtime_ticks / (double) xtimer_now() * 100;
+//             int switches = sched_pidlist[i].schedules;
+// #endif
+//             printf("\t%3" PRIkernel_pid
+// #ifdef DEVELHELP
+//                    " | %-20s"
+// #endif
+//                    " | %-8s %.1s | %3i"
+// #ifdef DEVELHELP
+//                    " | %5i (%5i) | %p "
+// #endif
+// #ifdef MODULE_SCHEDSTATISTICS
+//                    " | %6.3f%% |  %8d"
+// #endif
+//                    "\n",
+//                    p->pid,
+// #ifdef DEVELHELP
+//                    p->name,
+// #endif
+//                    sname, queued, p->priority
+// #ifdef DEVELHELP
+//                    , p->stack_size, stacksz, p->stack_start
+// #endif
+// #ifdef MODULE_SCHEDSTATISTICS
+//                    , runtime_ticks, switches
+// #endif
+//                   );
         }
     }
 
 #ifdef DEVELHELP
-    printf("\t%5s %-21s|%13s%6s %5i (%5i)\n", "|", "SUM", "|", "|",
-           overall_stacksz, overall_used);
+    printf("%5i %5i\n", overall_stacksz, overall_used);
 #endif
 }
